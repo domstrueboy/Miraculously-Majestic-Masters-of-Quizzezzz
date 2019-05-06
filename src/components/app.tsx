41ea64f4f5f99b3d9853a9f0c5cabb28f791b120
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-05-07 00:52:28 +0500

    Убрал отображение header

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,7 +3,7 @@ import { Route, Router, RouterOnChangeArgs } from "preact-router";
 
 import Home from "../routes/home";
 import Profile from "../routes/profile";
-import Header from "./header";
+// import Header from "./header";
 
 if ((module as any).hot) {
     // tslint:disable-next-line:no-var-requires
@@ -19,11 +19,12 @@ export default class App extends Component {
     public render() {
         return (
             <div id="app">
-                <Header />
+                {/* <Header /> */}
                 <Router onChange={this.handleRoute}>
                     <Route path="/" component={Home} />
-                    <Route path="/profile/" component={Profile} user="me" />
-                    <Route path="/profile/:user" component={Profile} />
+                    {/* <Route path="/profile/" component={Profile} user="me" /> */}
+                    {/* <Route path="/profile/:user" component={Profile} /> */}
+                    <Route path="/admin" component={Profile} user="admin" />
                 </Router>
             </div>
         );
