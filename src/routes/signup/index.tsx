2788f15a86cdddec9832403e835a1e259686fcd7
Pavel Bobylyov <domstrueboy@yandex.ru>
2019-05-14 01:42:15 +0500

    Styles were added to Button component, form was stylized

--- src/routes/signup/index.tsx
+++ src/routes/signup/index.tsx
@@ -1,7 +1,9 @@
 import { Component, h } from "preact";
-import { Link } from "preact-router/match";
 import * as style from "./style.css";
 
+import { Link } from "preact-router/match";
+
+import Button from "../../components/button";
 import InputField from "../../components/inputField";
 
 interface Props {}
@@ -9,12 +11,15 @@ export default class SignUp extends Component<Props> {
     public render() {
         return (
             <div class={style.signup}>
-                <h1>Registration Form</h1>
-                <InputField type="text" label="Your name" />
-                <InputField type="email" label="Your email" />
-                <InputField type="password" label="Password" />
-                <Link activeClassName={style.active} href="/">
-                    <button>Submit</button>
+                <form action="post">
+                    <h1>Registration Form</h1>
+                    <InputField type="text" label="Your name" />
+                    <InputField type="email" label="Your email" />
+                    <InputField type="password" label="Password" />
+                    <Button text="Sign Up"/>
+                </form>
+                <Link activeClassName={style.active} href="/signin">
+                    Already registered?
                 </Link>
             </div>
         );
